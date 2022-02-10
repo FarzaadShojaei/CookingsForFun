6f807b2642d6f18a8427d2bc066b32153a67c77f
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-02-10 16:40:49 +0330

    Testing Django App

---
+++ task1/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('Hello,World!')
