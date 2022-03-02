31e0c5e12f84bbace17e0d7672c9343833cb02ca
TheyCalledmeMrGlass <shojaeifarzad79@yahoo.com>
2022-03-02 09:40:49 +0330

    Adding SingleViewList

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,10 @@
-from urllib import request
+from reviews.models import Review
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import ReviewForm
 from django.views import View
 from django.views.generic.base import TemplateView
+
+from .forms import ReviewForm
 from .models import Review
 
 # Create your views here.
@@ -11,7 +12,6 @@ from .models import Review
 
 class ReviewView(View):
     def get(self, request):
-
         form = ReviewForm()
 
         return render(request, "reviews/review.html", {
@@ -20,6 +20,7 @@ class ReviewView(View):
 
     def post(self, request):
         form = ReviewForm(request.POST)
+
         if form.is_valid():
             form.save()
             return HttpResponseRedirect("/thank-you")
@@ -29,12 +30,12 @@ class ReviewView(View):
         })
 
 
-class ThankYouView(View):
+class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['message'] = "This Works"
+        context["message"] = "This works!"
         return context
 
 
@@ -45,4 +46,15 @@ class ReviewsListView(TemplateView):
         context = super().get_context_data(**kwargs)
         reviews = Review.objects.all()
         context["reviews"] = reviews
+        return context
+
+
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
         return context
